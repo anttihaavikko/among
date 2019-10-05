178e6c81a52cfcbe15dc0a5d7c3ecb3af363630b
Antti Haavikko <antti.haavikko@focus.fi>
2019-10-06 01:51:27 +0300

    Add emotion triggers

---
+++ Assets/Scripts/Emoter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Emoter : MonoBehaviour
+{
+    public Face face;
+    public Face.Emotion emotion;
+    public Face.Emotion leaveEmotion = Face.Emotion.Default;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        face.Emote(emotion);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        face.Emote(leaveEmotion);
+    }
+}
