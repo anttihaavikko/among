ca86b2b929272d82c5944571e42d17e42245ef4c
Antti Haavikko <antti.haavikko@focus.fi>
2019-10-06 00:48:34 +0300

    Add face parallax scrolling

---
+++ Assets/Scripts/FakeParallax.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeParallax : MonoBehaviour
+{
+    private Transform cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var v = cam.position * (1f - transform.position.z / 50f);
+        transform.position = new Vector3(v.x, v.y, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    private float speed;
+    private float angle;
+
+    private void Start()
+    {
+        angle = Random.value * 360f;
+        var s = Random.Range(0.01f, 0.02f);
+        speed = Random.value < 0.5f ? s : -s;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        angle += speed * Time.deltaTime * 60f;
+        transform.localRotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+
+    public void ChangeSpeed(float s)
+    {
+        speed = s;
+    }
+}
